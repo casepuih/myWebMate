3cdee998b3d2dd1ac866e7be98b40af6033373f1
Quentin Mousset <quentin@pharmasimple.com>
2023-03-02 13:44:20 +0100

    add port to sequelize

--- backend/utils/sql.utils.js
+++ backend/utils/sql.utils.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2/promise');
 const {TasksDTO} = require("../dto/tasksDTO");
 
-const { DB_SERVER, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
+const { DB_SERVER, DB_DATABASE, DB_USERNAME, DB_PASSWORD, DB_PORT } = process.env;
 
 const sql = mysql.createConnection({
     host: DB_SERVER,
     user: DB_USERNAME,
     password: DB_PASSWORD,
-    database: DB_DATABASE
+    database: DB_DATABASE,
+    port: DB_PORT
 });
 
 module.exports = sql;
