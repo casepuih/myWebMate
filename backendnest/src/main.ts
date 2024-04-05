c66546b3fd5871c479d64a689627d3c630ae0e56
Omer Akkoyunlu <omergt92@gmail.com>
2024-04-05 15:58:05 +0200

    added ValidationExceptionFilter, returns detailed info on fields failing validation

--- backendnest/src/main.ts
+++ backendnest/src/main.ts
@@ -3,11 +3,15 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { validationExceptionFactory } from './exceptions/validation.exception';
 import { HttpExceptionFilter } from './filters/http-exception/http-exception.filter';
+import { ValidationExceptionFilter } from './filters/validation-exception/validation-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
-  app.useGlobalFilters(new HttpExceptionFilter())
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+    new ValidationExceptionFilter()
+  )
   app.useGlobalPipes(new ValidationPipe({
     errorHttpStatusCode: 422,
     exceptionFactory: validationExceptionFactory
