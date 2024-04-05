ec62a26974e00c44d26a6608276ad99ebc87d9d7
Omer Akkoyunlu <omergt92@gmail.com>
2024-04-05 13:34:48 +0200

    added FriendsController to members.module

--- backendnest/src/modules/members/members.module.ts
+++ backendnest/src/modules/members/members.module.ts
@@ -3,10 +3,11 @@ import { MembersService } from './members.service';
 import { MembersController } from './members.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Member } from './entities/member.entity';
+import { FriendsController } from './friends.controller';
 
 @Module({
   imports:[TypeOrmModule.forFeature([Member])],
-  controllers: [MembersController],
+  controllers: [MembersController, FriendsController],
   providers: [MembersService],
   exports: [TypeOrmModule, MembersService]
 })
